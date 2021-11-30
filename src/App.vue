a24b3e149d2f531a6a62c5d48a2f6cbc40260fb2
simarbagga401 <Sahil111020@gmail.com>
2021-11-30 15:38:28 +0530

    feat: project setup complete

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Business Card Generator!</h1>
+  <BusinessCard />
+</template>
+
+<script setup>
+import BusinessCard from "./components/BusinessCard.vue";
+</script>
+
+<style>
+#app {
+  font-family: "Courier New", Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c2c2c;
+  margin-top: 60px;
+}
+</style>
